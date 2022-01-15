a6fb12557cd8bab12cd2a01909860142418cda0a
Đỗ Vinh Hà <vinhhado1401@gmail.com>
2022-01-15 13:50:12 +0700

    add comment utils file

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import { store } from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
+
+// hàm chuyển đổi dữ liệu file text sang dạng array object
 export const convertData = (data, setDataConvered, caculate) => {
   let tempConvertData = [];
   data.map((item, index) => {
@@ -8,6 +10,11 @@ export const convertData = (data, setDataConvered, caculate) => {
       .slice(item.indexOf(" ") + 1, item.indexOf(" => "))
       .split(", ");
     const result = item.slice(item.indexOf(" => ") + 4, item.indexOf(" = "));
+    // vd: r1: a, b, c => p = ( a + b + c ) / 2
+    // hypothesis là giả thuyết của luật lấy từ đầu cho đến "=>"
+    // hypothesis = ["a", "b", "c"]
+    // result là kết quả của luật lấy từ "=>" cho đến "="
+    // result = "p"
     const obj = {
       index: index,
       a: false,
@@ -27,11 +34,29 @@ export const convertData = (data, setDataConvered, caculate) => {
     hypothesis.forEach((val, inVal) => {
       obj[val] = true;
     });
+    // const obj = {
+    //   index: index,
+    //   a: true,
+    //   b: true,
+    //   c: true,
+    //   ha: false,
+    //   hb: false,
+    //   hc: false,
+    //   A: false,
+    //   B: false,
+    //   C: false,
+    //   S: false,
+    //   p: false,
+    //   result: "p",
+    //   distance: -1,
+    // };
     tempConvertData.push(obj);
+    // tempConvertData là mảng chứa các đối tượng (obj) luật
   });
   setDataConvered(tempConvertData);
 };
 
+// hàm tính toán đưa ra các luật thỏa mãn yêu cầu
 export const handleCalculate = (
   arrHypothesis,
   dataConvered,
@@ -39,8 +64,8 @@ export const handleCalculate = (
   setSATResult,
   setHypoDistance,
   setArrTable
-  // arrTable
 ) => {
+  // trước khi tính cần tính khoảng cách từ kết luận của các luật đến KL
   const tempDataConvered = handleCalculateDistanceEachRules(
     caculate,
     dataConvered
@@ -50,6 +75,8 @@ export const handleCalculate = (
   let tempHypothesisConvert = [];
   let SAT = [];
   let SATUsed = [];
+  // tách giả thuyết của bài toán arrHypothesis thành mảng
+  // vd: ["a", "b", "c"]
   arrHypothesis.map((item, index) => {
     item.map((val, inVal) => {
       if (val.value !== "") {
@@ -70,6 +97,7 @@ export const handleCalculate = (
   setSATResult(SATResult);
 };
 
+// hàm đệ quy tính toán trả ra các luật thỏa mãn điều kiện
 const callbackCaculate = (
   tempHypothesisConvert,
   SAT,
@@ -82,19 +110,22 @@ const callbackCaculate = (
   const arrHypothesisCurrent = [];
   if (!_.isEmpty(dataConvered)) {
     dataConvered.map((item, index) => {
+      // nếu luật đã được duyệt thì return bỏ qua
       if (
         !_.isEmpty(indexApproved) &&
         indexApproved.indexOf(item.index) !== -1
       ) {
         return;
       }
-      let check = 0;
-      let checkRequire = 0;
-      let checkResult = false;
+      let check = 0; // số thuộc tính = true và bằng giả thuyết
+      let checkRequire = 0; // số thuộc tính = true
+      let checkResult = false; // kiểm tra xem giả thuyết có chứa kết luận của luật không
+      // duyệt obj
       Object.keys(item).map((key, inKey) => {
         if (item[key] === true) {
           checkRequire++;
         }
+        // duyệt giả thuyết được cho bởi bài toán
         tempHypothesisConvert.map((val, inVal) => {
           if (item[key] === true && key === val) {
             check++;
@@ -114,19 +145,21 @@ const callbackCaculate = (
         SAT.push(item);
       }
     });
-
+    // kiểm tra arrHypothesisCurrent có chứa luật thỏa mãn điều kiện
     if (!_.isEmpty(arrHypothesisCurrent)) {
-      let indexMinDistance = 0;
-      let indexHypothesisCurrent = arrHypothesisCurrent[0].index;
-      let valueMinDistance = arrHypothesisCurrent[0].distance;
-      let checkResult = false;
-      let indexResult = -1;
+      let indexMinDistance = 0; // vị trí luật chứa khoảng cách bé nhất
+      let indexHypothesisCurrent = arrHypothesisCurrent[0].index; // vị trí luật chứa khoảng cách bé nhất hiện tại
+      let valueMinDistance = arrHypothesisCurrent[0].distance; // giá trị khoảng cách bé nhất hiện tại
+      let checkResult = false; // kiểm tra khoảng cách của luật
+      let indexResult = -1; // vị trí của luật chứa KL
       arrHypothesisCurrent.every((item, index) => {
+        // kiểm tra khoảng cách của luật. nếu khoảng cách = 0 thì dừng luôn
         if (item.distance === 0) {
           checkResult = true;
           indexResult = index;
           return false;
         }
+        // ngược lại thì gán khoảng cách min
         if (valueMinDistance > item.distance) {
           indexMinDistance = index;
           indexHypothesisCurrent = item.index;
@@ -134,7 +167,9 @@ const callbackCaculate = (
         }
         return true;
       });
+      // thêm các vị trí đã duyệt vào indexApproved để sau đó đệ quy thì check để bỏ qua luật này
       indexApproved.push(indexHypothesisCurrent);
+      // nếu giả thuyết chứa kết luận thì trả về mảng chứa các luật đã được sử dụng và luật đang được duyệt
       if (checkResult) {
         arrTable.push({
           arrHypoConvert: [...tempHypothesisConvert],
@@ -144,6 +179,7 @@ const callbackCaculate = (
         setArrTable(arrTable);
         return [...SATUsed, arrHypothesisCurrent[indexResult]];
       }
+      // ngược lại tiếp tục đệ quy
       SATUsed = [...SATUsed, arrHypothesisCurrent[indexMinDistance]];
       arrTable.push({
         arrHypoConvert: [...tempHypothesisConvert],
@@ -184,6 +220,7 @@ const callbackCaculate = (
   }
 };
 
+// hàm tính khoảng cách từ kết luận của các luật đến KL
 export const handleCalculateDistanceEachRules = (caculate, dataConvered) => {
   let tempDataConvert = [...dataConvered];
   dataConvered.map((item, index) => {
